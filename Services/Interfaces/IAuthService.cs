92c8a9e3ee3071ecc14f12da004538fbbe69221c
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-01 22:03:59 +0700

    Update IAuthService.cs

--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,1 +1,11 @@
+using PetCareManager.Models;
+using PetCareManager.DTOs;
 
+namespace PetCareManager.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        User Login(string username, string password);
+        User Register(User user);
+    }
+}
