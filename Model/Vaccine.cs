3ba090fa3ff51fd6bb9d325cadb2696b5eb15bc5
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-01 21:56:20 +0700

    Add Vaccine model with properties for vaccination data

--- Model/Vaccine.cs
+++ Model/Vaccine.cs
@@ -1,1 +1,12 @@
-
+namespace PetCareManager.Models
+{
+    public class Vaccine
+    {
+        public int VaccineID { get; set; }
+        public string VaccineName { get; set; }
+        public string ForSpecies { get; set; }
+        public string PreventDisease { get; set; }
+        public int? RecommendedIntervalMonths { get; set; }
+        public string Note { get; set; }
+    }
+}
