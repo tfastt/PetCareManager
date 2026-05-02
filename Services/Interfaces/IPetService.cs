84dc57fc417b03bffea82c89ec1ca5b60ac5e9de
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-03 00:53:43 +0700

    Update IPetService.cs

--- Services/Interfaces/IPetService.cs
+++ Services/Interfaces/IPetService.cs
@@ -1,1 +1,10 @@
+using System.Collections.Generic;
+using PetCareManager.Model;
 
+public interface IPetService
+{
+    List<Pet> GetPets(User user);
+    void AddPet(Pet pet, User user);
+    void UpdatePet(Pet pet, User user);
+    void DeletePet(int petId, User user);
+}
