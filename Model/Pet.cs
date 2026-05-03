5bd993ecf07d2a6d89357299d0efb50716a95234
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-04 01:37:35 +0700

    Update Pet.cs

--- Model/Pet.cs
+++ Model/Pet.cs
@@ -1,16 +1,22 @@
 namespace PetCareManager.Model
 {
+    public enum Gender
+    {
+        Male,
+        Female
+    }
     public class Pet
     {
         public int PetID { get; set; }
-        public string PetName { get; set; }
+        public string PetName { get; set; } = null!;
         public string Species { get; set; }
         public string Breed { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public double? Weight { get; set; }
         public string HealthStatus { get; set; }
         public int UserID { get; set; }
-        public string Gender { get; set; }
+        public User User { get; set; }
+        public Gender Gender { get; set; }
         public string Note { get; set; }
     }
 }
