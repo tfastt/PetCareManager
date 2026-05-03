7bc180c64f1ba408a1624a10622b9c0f8b53f442
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-03 16:00:36 +0700

    Update AppDbContext.cs

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -1,1 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using PetCareManager.Model;
 
+namespace PetCareManager.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Vaccine> Vaccines { get; set; }
+        public DbSet<VaccinationSchedule> VaccinationSchedules { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.\SQLEXPRESS;Database=PetCareDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {  
+            modelBuilder.Entity<User>().ToTable("User");
+            
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+            
+            modelBuilder.Entity<Pet>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Pets)
+                .HasForeignKey(p => p.UserID);
+            
+            modelBuilder.Entity<VaccinationSchedule>()
+                .HasOne(vs => vs.Pet)
+                .WithMany()
+                .HasForeignKey(vs => vs.PetID);
+        }
+    }
+}
