5541c5c319e63ab78f4d3b09c0020ee32b0b51a5
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-04 05:02:21 +0700

    Update AuthService.cs

--- Services/Implementations/AuthService.cs
+++ Services/Implementations/AuthService.cs
@@ -1,40 +1,28 @@
 using PetCareManager.Model;
 using PetCareManager.Services.Interfaces;
+using PetCareManager.DataAccess;
 
 namespace PetCareManager.Services.Implementations
 {
     public class AuthService : IAuthService
     {
-        private List<User> _users = new List<User>();
+        private UserRepository _userRepository;
 
-public User Login(string email, string password)
-{
-    var user = _userRepository.GetUserByEmail(email);
-
-    if (user == null)
-        throw new Exception("User not found");
-
-    if (user.PasswordHash != password) // demo trước, chưa hash
-        throw new Exception("Wrong password");
-
-    return user;
-}
-
-        public User Register(string username, string password)
+        public AuthService(UserRepository userRepository)
         {
-            if (_users.Any(u => u.UserName == username))
-                throw new Exception("User already exists");
+            _userRepository = userRepository;
+        }
 
-            var user = new User
-            {
-                UserID = _users.Count + 1,
-                UserName = username,
-                PasswordHash = password,
-                Role = "Owner",
-                CreatedDate = DateTime.Now
-            };
+        public User Login(string email, string password)
+        {
+            var user = _userRepository.GetUserByEmail(email);
 
-            _users.Add(user);
+            if (user == null)
+                throw new Exception("User not found");
+
+            if (user.PasswordHash != password) // demo: chưa hash
+                throw new Exception("Wrong password");
+
             return user;
         }
     }
