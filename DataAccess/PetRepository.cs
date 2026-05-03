e9c6039af416ebd4b0e5a3ea1ad6c6da3db2044a
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-04 01:14:48 +0700

    Update PetRepository.cs

--- DataAccess/PetRepository.cs
+++ DataAccess/PetRepository.cs
@@ -6,7 +6,7 @@ using PetCareManager.Model;
 
 namespace PetCareManager.DataAccess
 {
-    public class PetService
+    public class PetRepository
     {
         private string connectionString =
             "Server=localhost\\SQLEXPRESS;Database=PetCareManagement;Trusted_Connection=True;";
