a2f6e1984750daeaf60e3c9c4119b45e501ca35c
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-03 16:13:58 +0700

    Update AppDbContext.cs

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -1,1 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PetCareManager.Model;
 
+public class AppDbContext : DbContext
+{
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=.;Database=PetCareDB;Trusted_Connection=True;");
+    }
+}
