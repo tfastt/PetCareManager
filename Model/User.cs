0b5436b468cbd0bcd005d0813eb979e1584b994d
Thành Thuận <thuannt3844@ut.edu.vn>
2026-05-04 01:41:27 +0700

    Update User.cs

--- Model/User.cs
+++ Model/User.cs
@@ -3,13 +3,14 @@ namespace PetCareManager.Model
     public class User
     {
         public int UserID { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set; } = null!;
         public string Phone { get; set; }
         public string Address { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
         public string Role { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Note { get; set; }
+        public List<Pet> Pets { get; set; }
     }
 }
